2190bca8ac96c1373db97cffd5d230d03f588876
stopstress <controllingstress@yandex.ru>
2019-06-22 11:02:15 +0300

    Dz3 190622 1101 start

---
+++ app/src/main/java/by/stress/radyukandroidpvt/dz3/Dz3Activity.kt
@@ -0,0 +1,13 @@
+package by.stress.radyukandroidpvt.dz3
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import by.stress.radyukandroidpvt.R
+
+class Dz3Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dz3)
+    }
+}
