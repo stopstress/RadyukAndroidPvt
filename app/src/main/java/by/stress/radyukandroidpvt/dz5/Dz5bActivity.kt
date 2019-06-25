f31bfb62a0278cbab09ac0bed300193751dd3bcc
stopstress <controllingstress@yandex.ru>
2019-06-25 23:36:12 +0300

    Dz5 190625 2330 blinkingOwl

--- app/src/main/java/by/stress/radyukandroidpvt/dz5/Dz5bActivity.kt
+++ app/src/main/java/by/stress/radyukandroidpvt/dz5/Dz5bActivity.kt
@@ -2,12 +2,17 @@ package by.stress.radyukandroidpvt.dz5
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.drawable.AnimationDrawable
 import by.stress.radyukandroidpvt.R
+import kotlinx.android.synthetic.main.activity_dz5b.*
+
 
 class Dz5bActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz5b)
+
+        (blinkingOwlImageView.getBackground() as AnimationDrawable).start()
     }
 }
