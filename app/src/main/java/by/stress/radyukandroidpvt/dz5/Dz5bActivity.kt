324ceb546b350c69dc90e6ebf9beefb346622ff3
stopstress <controllingstress@yandex.ru>
2019-06-29 16:06:49 +0300

    Dz5 190629 1606 start-stop animation

--- app/src/main/java/by/stress/radyukandroidpvt/dz5/Dz5bActivity.kt
+++ app/src/main/java/by/stress/radyukandroidpvt/dz5/Dz5bActivity.kt
@@ -2,6 +2,7 @@ package by.stress.radyukandroidpvt.dz5
 
 import android.os.Bundle
 import android.graphics.drawable.AnimationDrawable
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import by.stress.radyukandroidpvt.R
 import kotlinx.android.synthetic.main.activity_dz5b.*
@@ -11,7 +12,17 @@ class Dz5bActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dz5b)
+    }
 
-        (blinkingOwlImageView.getBackground() as AnimationDrawable).start()
+    override fun onResume() {
+        super.onResume()
+        (blinkingOwlImageView.background as AnimationDrawable).start()
+        Log.d("Dz5b", "start animation")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        (blinkingOwlImageView.background as AnimationDrawable).stop()
+        Log.d("Dz5b", "stop animation")
     }
 }
